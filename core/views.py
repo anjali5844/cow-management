ae6f858c2ee31f0fcd8bda2760338bb70685af78
anjali thakur <at000000097@gmail.com>
2025-12-01 20:19:28 +0530

    UI updated.

--- core/views.py
+++ core/views.py
@@ -2,7 +2,25 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'core/index.html')
+
+	from .models import About, Service, Feature, Product, Team, Testimonial, Gallery
+	about = About.objects.first()
+	services = Service.objects.all()
+	features = Feature.objects.all()
+	products = Product.objects.order_by('-id')[:4]
+	team = Team.objects.all()
+	testimonials = Testimonial.objects.all()
+	gallery = Gallery.objects.all()
+	context = {
+		'about': about,
+		'services': services,
+		'features': features,
+		'products': products,
+		'team': team,
+		'testimonials': testimonials,
+		'gallery': gallery,
+	}
+	return render(request, 'core/index.html', context)
 
 def about(request):
 	return render(request, 'core/about.html')
@@ -17,16 +35,24 @@ def gallery(request):
 	return render(request, 'core/gallery.html')
 
 def service(request):
-	return render(request, 'core/service.html')
+	from .models import Service
+	services = Service.objects.all()
+	return render(request, 'core/service.html', {'services': services})
 
 def team(request):
-	return render(request, 'core/team.html')
+	from .models import Team
+	team = Team.objects.all()
+	return render(request, 'core/team.html', {'team': team})
 
 def testimonial(request):
-	return render(request, 'core/testimonial.html')
+	from .models import Testimonial
+	testimonials = Testimonial.objects.all()
+	return render(request, 'core/testimonial.html', {'testimonials': testimonials})
 
 def product(request):
-	return render(request, 'core/product.html')
+	from .models import Product
+	products = Product.objects.all()
+	return render(request, 'core/product.html', {'products': products})
 
 def notfound(request, exception=None):
 	return render(request, '404.html', status=404)
--- cowproject/settings.py
+++ cowproject/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
@@ -132,6 +133,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'core' / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files (User uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
